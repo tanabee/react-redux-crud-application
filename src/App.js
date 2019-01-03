7af1f5ec36d76a90fdf2ab6ac5549291b8b42cb5
tanabee <tanabee1986@gmail.com>
2019-01-04 02:36:38 +0900

    Updated jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>;
+    //const greeting = 'Hello, world!';
+    //const dom = <h1 className="foo">{greeting}</h1>;
+    //return dom;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={() => {console.log('I am clicked.')}} />
+      </React.Fragment>
+    )
   }
 }
 
