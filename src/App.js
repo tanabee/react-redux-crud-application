284c06c8ed1640dfe4dbbc509a46585c1ccd0036
tanabee <tanabee1986@gmail.com>
2019-01-04 14:58:49 +0900

    Refresh

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles = [
-    { name: 'Ichiro', age: 10 },
-    { name: 'Jiro', age: 8 },
-    { name: 'Saburo', age: 6 },
-    { name: 'Shiro', age: 4 },
-    { name: 'Goro' },
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
-
-User.defaultProps = {
-  age: 0
-}
-
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  age : PropTypes.number
+class Counter extends Component {
+  render() {
+    return (<div>Counter</div>)
+  }
 }
 
 export default App;
