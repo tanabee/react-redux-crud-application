f1bd2c2139f0cf8cedb808e7d7d4b9629a7a4edd
tanabee <tanabee1986@gmail.com>
2019-01-04 15:11:23 +0900

    Added state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import React, { Component } from 'react';
 const App = () => (<Counter></Counter>)
 
 class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+    console.log(this.state)
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
   render() {
-    return (<div>Counter</div>)
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
   }
 }
 
