b28803f7dc4370b71a4a312a87352ce09c2f8ae9
tanabee <tanabee1986@gmail.com>
2019-01-04 14:42:59 +0900

    User map function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
 const App = () => {
+  const profiles = [
+    { name: 'Ichiro', age: 10 },
+    { name: 'Jiro', age: 8 },
+    { name: 'Saburo', age: 6 },
+    { name: 'Shiro', age: 4 },
+    { name: 'Goro' },
+  ]
   return (
     <div>
-      <User name={"Ichiro"} age={10}/>
-      <User name={'Jiro'} age={8}/>
-      <User name={'Saburo'} age={6}/>
-      <User name={'Shiro'} age={4}/>
-      <User name={'Goro'} age={2}/>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
-  );
+  )
 }
 
 const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
