61480797c055ca7eb4019b26d6ba7b9dedf33207
tanabee <tanabee1986@gmail.com>
2019-01-05 15:55:51 +0900

    GET events

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { increment, decrement } from '../actions'
+import { readEvents } from '../actions'
 
-class App extends Component {
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+
   render() {
     const props = this.props
     return (
@@ -15,12 +19,12 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = state => ({ value: state.count.value })
+const mapStateToProps = state => ({})
 //const mapDispatchToProps = dispatch => ({
 //  increment: () => dispatch(increment()),
 //  decrement: () => dispatch(decrement())
 //})
 
-const mapDispatchToProps = ({ increment, decrement })
+const mapDispatchToProps = ({ readEvents })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
