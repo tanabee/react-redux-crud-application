565ff69d393a348a079ce17f6f9d9a6a04a3bea1
tanabee <tanabee1986@gmail.com>
2019-01-06 19:52:38 +0900

    Updates

---
+++ precache-manifest.deeec526387d1de4a300e04b59ba9d44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2ff8b337c8740f861bf",
+    "url": "/static/js/main.b2ff8b33.chunk.js"
+  },
+  {
+    "revision": "a6b1e638de1c068437f3",
+    "url": "/static/js/1.a6b1e638.chunk.js"
+  },
+  {
+    "revision": "b2ff8b337c8740f861bf",
+    "url": "/static/css/main.a160e5f9.chunk.css"
+  },
+  {
+    "revision": "57e1f3f7f231fed090f059002c90b925",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-redux-crud-application/precache-manifest.910273b738c950570a47e3c3132e09de.js"
+  "/precache-manifest.deeec526387d1de4a300e04b59ba9d44.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-redux-crud-application/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
